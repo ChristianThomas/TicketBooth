5e5d7eb421d5020d7cf83ed408ddcece8320cd03
william-penaayala <40260805+william-penaayala@users.noreply.github.com>
2020-02-19 22:33:23 -0500

    Update Main.java

--- TicketBooth/src/frontend/Main.java
+++ TicketBooth/src/frontend/Main.java
@@ -1,5 +1,18 @@
 package frontend;
 
 public class Main {
-
+	public static void main(String args[]) {
+		// Instantiating our state-keeping objects
+		InputParser inputParser = new InputParser();
+		Logic logic = new Logic();
+		
+		// Program loop
+		while(inputParser.hasNext()) {
+			Transaction transaction = inputParser.parseInput();
+			TransactionResult result = logic.processTransaction(transaction);
+			System.out.println(result.message());
+		}
+		
+		// Done
+	}
 }
