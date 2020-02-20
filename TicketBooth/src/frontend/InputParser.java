107968a6bfc479a6f5597495318564ac440b6dc5
william-penaayala <40260805+william-penaayala@users.noreply.github.com>
2020-02-19 23:27:42 -0500

    Started InputParser, works for Login and Logout

--- TicketBooth/src/frontend/InputParser.java
+++ TicketBooth/src/frontend/InputParser.java
@@ -5,8 +5,45 @@ import java.util.Scanner;
 public class InputParser {
 	Scanner stdin;
 	
+	InputParser() {
+		this.stdin = new Scanner(System.in);
+	}
+	
+	// Implemented so far: Login, Logout
 	public Transaction parseInput() {
-		
-		// pass
+		String in = stdin.next();
+		if(in.equals("login")) {
+			return parseLogin();
+		} 
+		else if (in.equals("logout")) {
+			return new LogoutTransaction("");
+		}
+		return null;
+	}
+	
+	// If you ever need a yes/no confirmation, use this
+	public boolean confirmAction(String message) {
+		System.out.println(message);
+		String in = stdin.next();
+		while(true) {
+			if(in.startsWith("y")) {
+				return true;
+			} else if (in.startsWith("n")) {
+				return false;
+			}
+		}
+	}
+	
+	// For the while loop in main
+	public boolean hasNext() {
+		return stdin.hasNext();
+	}
+	
+	// Returns a login transaction
+	private LoginTransaction parseLogin() {
+		System.out.print("Enter username: ");
+		String in = stdin.next();
+		// TODO: Error checking
+		return new LoginTransaction(in);
 	}
 }
